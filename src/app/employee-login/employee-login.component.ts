319e803424f29f92bee498ae740e9355bd36e86b
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-11 18:06:33 +0530

    add employee login

---
+++ src/app/employee-login/employee-login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-login',
+  templateUrl: './employee-login.component.html',
+  styleUrls: ['./employee-login.component.css']
+})
+export class EmployeeLoginComponent {
+
+  username=""
+  password=""
+
+
+  emplogin=()=>
+  {
+    let data:any={
+      "username":this.username,
+      "password":this.password
+    }
+  }
+
+}
